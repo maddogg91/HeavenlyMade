5e0a655a8ad7a6e51d5b5f4c48d04124217c9c9a
maddogg91 <40213091+maddogg91@users.noreply.github.com>
2023-06-19 14:51:47 +0000

    Node JS

---
+++ index.js
@@ -0,0 +1,91 @@
+var http = require('http');
+const express = require('express');
+const app = express();
+var path = require('path');
+var sass = require('node-sass');
+const axios = require('axios');
+const search= require('./book');
+const session= require('express-session');
+const flash= require('connect-flash');
+
+app.set('views', path.join(__dirname, 'templates'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.use(session({
+    secret:'flashblog',
+    saveUninitialized: true,
+    resave: true
+}));
+app.use(express.static('public'));
+app.use(express.static('images'));
+app.use(flash());
+
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database(':memory:');
+
+
+const server = app.listen(7000, () => {
+  console.log(`Express running → PORT ${server.address().port}`);
+});
+
+db.serialize(() => {
+    db.run("CREATE TABLE books (title TEXT)");
+
+    const stmt = db.prepare("INSERT INTO books VALUES (?)");
+    stmt.run("Look Again");
+	stmt.run("Ministering Through Cliches Volume 1");
+	stmt.run("Ministering Through Cliches Volume 2");
+	stmt.run("Poems of Delight: Ministering to your Soul");
+	stmt.run("Poems of Delight: To Meet Your Comfort");
+    stmt.finalize();
+
+    db.each("SELECT rowid AS id, title FROM books", (err, row) => {
+        console.log(row.id + ": " + row.title);
+    });
+});
+
+
+
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/books', (req, res) => {
+  const books = [];  
+  const data = [];
+  title= req.query.title;		
+  if (title != undefined){
+	   db.all("SELECT title FROM books WHERE title LIKE $title", {$title: '%' + title + '%'},
+	   (err, rows) => {rows.forEach((row) =>{
+		 
+		   books.push(row.title);
+	   })		      
+  });
+setTimeout(() => { 
+if (books.length > 0){
+ books.forEach((t) => {
+	    data.push(search.findBook(t));
+	 
+  });
+   res.render('results', {books: JSON.stringify(data)});
+}
+
+else{
+ req.flash('message',"No results found, press the back button to return to books page and try again");
+ res.send(req.flash('message'));
+
+}
+ }, 5000);
+ 
+ 
+  }
+  else{
+  res.render('book');
+  }
+});
+
+app.get('/ministering', (req, res) => {
+  res.render('cliches');
+});
+
